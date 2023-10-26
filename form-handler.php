a0eddca835f4069fa3b8664ea6a88bc839fe5f8e
stjohnschurch <148819074+stjohnschurch@users.noreply.github.com>
2023-10-26 20:39:41 +0100

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -11,13 +11,14 @@ $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
      "User Email: $visitor_email.\n".
      "Subject: $subject.\n".
-     "User Message: $message.\n";
+     "User Message: $message .\n";
 
 $to = 'harrygreatrex25@gmail.com';
+
 $headers = "From: $email_from \r\n";
 
 
-$headers .= "Reply-To: $visitor_email \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
 
 
 mail($to,$email_subject,$email_body,$headers);
